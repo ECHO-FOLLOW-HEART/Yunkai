0268fcaffe48c62ffdc5a73ac2e25cf2cee53bb8
Zephyre <haizi.zh@gmail.com>
2015-07-02 23:34:22 +0800

    FIX: 配置文件读取相关的bug

--- src/main/scala/com/lvxingpai/yunkai/IdGenerator.scala
+++ src/main/scala/com/lvxingpai/yunkai/IdGenerator.scala
@@ -1,31 +1,14 @@
 package com.lvxingpai.yunkai
 
-import com.redis.RedisClientPool
+import com.lvxingpai.yunkai.service.RedisFactory
 import com.twitter.util.{Future, FuturePool}
-
-import scala.collection.JavaConversions._
 
 /**
  * 自增长ID生成器
  *
  */
 object IdGenerator {
-  lazy val redisPool = {
-
-    val conf = Global.conf
-    val redisEndpoints = conf.getConfig("backends.redis").entrySet().toSeq map (backend => {
-      val tmp = backend.getValue.unwrapped().toString.split(":")
-      val host = tmp(0)
-      val port = tmp(1).toInt
-      host -> port
-    })
-
-    val host = redisEndpoints.head._1
-    val port = redisEndpoints.head._2
-    val redisDB = Global.conf.getInt("yunkai.redis.db")
-
-    new RedisClientPool(host, port, database = redisDB)
-  }
+  lazy val redisPool = RedisFactory.pool
 
   def generateId(key: String)(implicit futurePool: FuturePool): Future[Long] = futurePool {
     redisPool withClient {
