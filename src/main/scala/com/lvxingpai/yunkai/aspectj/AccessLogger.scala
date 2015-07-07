32212ae1121c5cbf437b2c95c84bef3844d72376
Zephyre <haizi.zh@gmail.com>
2015-07-07 15:04:54 +0800

    MOD: 调整AccessLogger
    在测试的情况下，不再启用AccessLogger

--- src/main/scala/com/lvxingpai/yunkai/aspectj/AccessLogger.scala
+++ src/main/scala/com/lvxingpai/yunkai/aspectj/AccessLogger.scala
@@ -1,5 +1,6 @@
 package com.lvxingpai.yunkai.aspectj
 
+import com.lvxingpai.yunkai.Global
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.{Aspect, Before}
 import org.slf4j.LoggerFactory
@@ -10,12 +11,17 @@ import org.slf4j.LoggerFactory
  */
 @Aspect
 class AccessLogger {
+  private val runlevel = Global.conf.getString("runlevel")
+
   @Before("execution(com.twitter.util.Future com.lvxingpai.yunkai.handler.UserServiceHandler..*(..))")
   def log(jp: JoinPoint): Unit = {
-    val signature = jp.getSignature
-    val message = s"Invoked: ${signature.toLongString}"
+    // 对应test的情况，不再记录日志
+    if (runlevel != "test") {
+      val signature = jp.getSignature
+      val message = s"Invoked: ${signature.toLongString}"
 
-    val logger = LoggerFactory.getLogger("access")
-    logger.info(message)
+      val logger = LoggerFactory.getLogger("access")
+      logger.info(message)
+    }
   }
 }
