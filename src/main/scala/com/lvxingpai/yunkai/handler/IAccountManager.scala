cbc4c78d7f7f034f7a0b3b60c3abe6a2fd23deeb
ECHO-FOLLOW-HEART <pengyt19890703@163.com>
2015-09-18 15:05:52 +0800

    UserServiceHaldlerTest添加getUserById测试

---
+++ src/main/scala/com/lvxingpai/yunkai/handler/IAccountManager.scala
@@ -0,0 +1,8 @@
+package com.lvxingpai.yunkai.handler
+
+/**
+ * Created by pengyt on 2015/9/18.
+ */
+trait IAccountManager {
+
+}
---
+++ src/test/scala/com/lvxingpai/yunkai/UserServiceHandlerTest.scala
@@ -0,0 +1,8 @@
+package com.lvxingpai.yunkai
+
+/**
+ * Created by pengyt on 2015/9/18.
+ */
+class UserServiceHandlerTest {
+
+}
